4ff55f032729f085f90fc27bce16f0d2fe06fec9
Yi Chien Lee <yician.lee@gmail.com>
2024-08-25 12:22:38 -0700

    add delete category functionality

--- ecommerce/src/main/java/org/ecommerce/project/controller/CategoryController.java
+++ ecommerce/src/main/java/org/ecommerce/project/controller/CategoryController.java
@@ -2,10 +2,7 @@ package org.ecommerce.project.controller;
 import org.ecommerce.project.model.Category;
 import org.ecommerce.project.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,5 +22,11 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category created";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }
--- ecommerce/src/main/java/org/ecommerce/project/service/CategoryServiceImpl.java
+++ ecommerce/src/main/java/org/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,6 @@
-package org.ecommerce.project.service.service;
+package org.ecommerce.project.service;
 
 import org.ecommerce.project.model.Category;
-import org.ecommerce.project.service.CategoryService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,5 +20,18 @@ public class CategoryServiceImpl implements CategoryService {
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if (category == null) {
+            return "Category not found";
+        }
+
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " deleted";
     }
 }
