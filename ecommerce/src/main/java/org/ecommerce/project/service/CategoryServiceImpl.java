4e864ae307695deab3147519eff9636db0295e08
Yi Chien Lee <yician.lee@gmail.com>
2024-08-25 17:50:04 -0700

    add custom exceptions for api exception

--- ecommerce/src/main/java/org/ecommerce/project/service/CategoryServiceImpl.java
+++ ecommerce/src/main/java/org/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package org.ecommerce.project.service;
 
+import org.ecommerce.project.exceptions.APIException;
 import org.ecommerce.project.exceptions.ResourceNotFoundException;
 import org.ecommerce.project.model.Category;
 import org.ecommerce.project.repository.CategoryRepository;
@@ -26,6 +27,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        if (savedCategory != null) {
+            throw new APIException("Category with the name " + category.getCategoryName() + " already exists");
+        }
         category.setCategoryId(nextId++);
         categoryRepository.save(category);
     }
@@ -41,7 +46,6 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-
         Category savedCategory = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
 
