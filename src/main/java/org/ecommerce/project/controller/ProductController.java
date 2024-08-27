35ef2889fc4319eb615d85c65d1acf535fb5a6af
Yi Chien Lee <yician.lee@gmail.com>
2024-08-26 21:50:51 -0700

    add update image functionality

--- src/main/java/org/ecommerce/project/controller/ProductController.java
+++ src/main/java/org/ecommerce/project/controller/ProductController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -51,5 +54,12 @@ public class ProductController {
     public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId) {
         ProductDTO deletedProductDTO = productService.deleteProduct(productId);
         return new ResponseEntity<>(deletedProductDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(@RequestParam("image") MultipartFile image,
+                                                         @PathVariable Long productId) throws IOException {
+        ProductDTO updatedProductDTO = productService.updateProductImage(productId, image);
+        return new ResponseEntity<>(updatedProductDTO, HttpStatus.OK);
     }
 }
