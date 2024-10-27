ac5f5972b1dfe1d34b29f1e46d5a569683c2e257
amandayclee <yician.lee@gmail.com>
2024-10-26 23:21:15 -0700

    check loose

--- src/main/java/org/ecommerce/project/model/Address.java
+++ src/main/java/org/ecommerce/project/model/Address.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +45,7 @@ public class Address {
     @Size(min = 5, message = "Zipcode name must be at least 5 characters")
     private String zipcode;
 
+    @ToString.Exclude
     @ManyToMany(mappedBy = "addresses")
     private List<User> users = new ArrayList<>();
 
