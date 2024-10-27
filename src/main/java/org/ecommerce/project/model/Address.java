3123abedee097aec5a19e1e0f1616cb4232573e2
amandayclee <yician.lee@gmail.com>
2024-10-26 21:52:47 -0700

    add seller role in User and Address

--- src/main/java/org/ecommerce/project/model/Address.java
+++ src/main/java/org/ecommerce/project/model/Address.java
@@ -1,13 +1,58 @@
 package org.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message = "Street name must be at least 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be at least 5 characters")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 4, message = "City name must be at least 5 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 5, message = "State name must be at least 5 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 5, message = "Country name must be at least 5 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 5, message = "Zipcode name must be at least 5 characters")
+    private String zipcode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(String zipcode, String country, String state, String city, String buildingName, String street) {
+        this.zipcode = zipcode;
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.buildingName = buildingName;
+        this.street = street;
+    }
 }
